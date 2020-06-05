58ed34335f58e73a63879926a690797897e413d1
Jakubíček Roman (119710) <jakubicek@vutbr.cz>
2020-06-05 15:19:03 +0200

    validace preucene site

---
+++ TrainingMatlab/validace_net2.m
@@ -0,0 +1,33 @@
+%% validace nets
+
+clear all
+close all
+clc
+
+load('Trained_nets\Net_test.mat')
+
+%% datastore
+
+% imdsValid = imdsTestComb.UnderlyingDatastores{1};
+% labels = imdsTestComb.UnderlyingDatastores{2}.Files;
+% labels = cellfun(@(x) ReaderValid(x), labels,'UniformOutput',false);
+
+imdsValid = imdsTrainComb.UnderlyingDatastores{1};
+labels = imdsTrainComb.UnderlyingDatastores{2}.Files;
+labels = cellfun(@(x) ReaderValid(x), labels,'UniformOutput',false);
+
+%% Prediction
+
+pred = predict(net, imdsValid);
+
+%% statistic
+
+GT = cell2mat(labels);
+% pred = round(pred);
+
+RMSE = sqrt( sum(((GT - pred).^2),'All')/(numel(GT)) )
+
+ACC = sum( sum( (GT == (pred)),2)==6) ./ size(pred,1) *100
+
+
+
